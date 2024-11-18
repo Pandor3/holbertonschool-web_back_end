037ea521163e07cea77618d845185de12f929685
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-11-18 11:45:03 +0100

    Added exercise 0 and its main file

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+function getListStudents(id, firstName, location) {
+    // This function will return an array of objects
+    id = ['1', '2', '3'];
+    firstName = ['Guillaume', 'James', 'Serena'];
+    location = ['San Francisco', 'Columbia', 'San Francisco'];
+    const studentlist = [id, firstName, location]
+    return studentlist
+}
