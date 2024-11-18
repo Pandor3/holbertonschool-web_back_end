511506349168784d65ea5b67659cf877ad0bc3cf
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-11-18 14:16:47 +0100

    Edited exercise 0 syntax

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 export default function getListStudents() {
     // This function will return an array of objects
     return [
-        "{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },",
-        "{ id: 2, firstName: 'James', location: 'Columbia' },",
-  "{ id: 5, firstName: 'Serena', location: 'San Francisco' }"
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' }
     ]
 }
