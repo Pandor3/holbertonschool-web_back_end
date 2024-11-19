73c461c09bc64123ff3f82c7a505e9070e762355
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-11-19 10:17:02 +0100

    Edited exercise 2 and its main file

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,3 @@
-import getListStudent from './0-get_list_students';
-
 export default function getStudentsByLocation(students, city) {
   // This function will return an array of locations
   return students.filter((student) => city === students.location);
