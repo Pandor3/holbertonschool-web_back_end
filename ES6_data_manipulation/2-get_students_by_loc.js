2466f7f2f505a5789bf50f73f8d113e39f5cd136
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-11-19 10:10:31 +0100

    Edited exercise 2 and its main file, removed extensions

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,7 +2,7 @@ import getListStudent from './0-get_list_students';
 
 export default function getStudentsByLocation() {
   // This function will return an array of locations
-	const students = getListStudent();
+  const students = getListStudent();
 
-  return students.filter((student) => student.location == 'San Francisco');
+  return students.filter((student) => student.location === 'San Francisco');
 }
