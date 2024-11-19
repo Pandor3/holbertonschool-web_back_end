67002c4a450cc67738941eede2e88719a33605f5
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-11-19 10:18:48 +0100

    Edited syntax of exercise 2

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 export default function getStudentsByLocation(students, city) {
   // This function will return an array of locations
-  return students.filter((student) => city === students.location);
+  return students.filter((students) => city === students.location);
 }
