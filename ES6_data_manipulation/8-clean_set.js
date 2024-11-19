41178de5679f12ed37101aecf6e432dc4c495361
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-11-19 14:35:41 +0100

    Added exercise 8 and its main file

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  if (!set || !startString || typeof startString !== 'string') {
+    return '';
+  }
+  return Array.from(set)
+    .filter((value) => typeof value === 'string'
+          && value.startsWith(startString))
+            .map((value) => value.slice(startString.length))
+            .filter((value) => value.length > 0)
+            .join('-');
+}
