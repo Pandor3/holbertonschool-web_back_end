f2dcb0e28095da0a593df2aa80b14cd4effc57aa
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-11-20 15:52:09 +0100

    Added exercise 9 and its main files

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+	const handleFunction = () => {
+    try {
+      return mathFunction();
+    } 
+		catch (error) {
+			return `Error: ${error.message}`;
+    }
+  };
+  const queue = [handleFunction(), 'Guardrail was processed'];
+  return queue;
+}
