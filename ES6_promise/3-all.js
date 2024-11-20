7edcfb42cb04b6101237fbe27e809ef792db2bb4
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-11-20 13:31:12 +0100

    Edited exercise 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, UserResponse]) => {
+    .then(([photoResponse, userResponse]) => {
       console.log(`${PhotoResult.body} ${UserResult.firstName} ${UserResult.lastName}`);
     })
     .catch(() => {
