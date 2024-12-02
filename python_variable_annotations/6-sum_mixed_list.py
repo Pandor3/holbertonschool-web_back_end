085028744a54c3f0ffce0bb6924a8d65c81ec858
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-12-02 15:03:07 +0100

    Edited exercise 6, inverted int and float

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -8,7 +8,7 @@ take a list of integers and float and then return its sum
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     This function will return a mix of integers and floats
     """
