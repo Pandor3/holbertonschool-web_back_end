42cbbe5d12143ed7f5b30238f8cdad979fca02db
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-12-02 14:31:10 +0100

    Added exercise 3 and its main file

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module will be used to take a
+n float and returns its string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This function converts a float to a string
+    """
+
+    return str(n)
