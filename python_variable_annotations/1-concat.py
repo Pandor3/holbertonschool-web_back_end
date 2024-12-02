37c050e5d618f7cdeafc7445943b011626ea3fd3
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-12-02 14:13:05 +0100

    Added exercise 1 and its main file

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module will concatenate two strings
+and return the concatenated strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function will concatenate two strings
+    """
+
+    return str1 + str2
