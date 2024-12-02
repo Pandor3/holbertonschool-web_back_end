b6948650745337b7432b21c88426ac82fb7a830d
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-12-02 14:44:41 +0100

    Added exercise 5 and its main file

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module will contain a function which
+will take a list and return its content
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    This function will return the sum of a list of floats
+    """
+    return sum(input_list)
