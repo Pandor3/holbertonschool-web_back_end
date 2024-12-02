ce103c3cd90b648d09b897515ef8d614a820c9dc
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-12-02 13:59:53 +0100

    Edited exercise 0 and its main file, added commentary

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-# This module will create a function named add
-# it will return the sum of two variables
+"""
+This module will create a function named add
+ and it will return the sum of two variables
+"""
+
 
 def add(a: float, b: float) -> float:
-    # This function will add two variables and return their sums #
+    """
+    This function will add two variables and return their sums
+    """
+
     return a + b
