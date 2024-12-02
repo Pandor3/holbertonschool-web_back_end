76a4fbb364fa487d8fd791ee4dfbf2816d149b98
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-12-02 14:20:57 +0100

    Added exercise 2 and its main file

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module will take a float argument and
+return what it will be used for
+"""
+
+import math
+
+
+def floor(n: float) -> float:
+    """
+    This function will return an n float as a boat floor
+    """
+
+    return math.floor(n)
