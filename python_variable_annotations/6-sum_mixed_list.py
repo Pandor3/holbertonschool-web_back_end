9816855cae9c981dbbafd72d4f412868fb74889c
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-12-02 14:58:48 +0100

    Added exercise 6 and its main file

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module will contain a function which will
+take a list of integers and float and then return its sum
+"""
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """
+    This function will return a mix of integers and floats
+    """
+
+    return float(sum(mxd_lst))
