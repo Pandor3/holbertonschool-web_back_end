8c106830389beabdea7765fd43d92bcc72ea1255
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-12-05 09:49:58 +0100

    Added exercise 0 and its main file

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module will contain an async function which will
+take an int argument and return its randomized values
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10):
+    """
+    This async function will delay a return at a random time
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
