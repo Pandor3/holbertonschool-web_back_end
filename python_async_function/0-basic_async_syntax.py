a246720006a9d818ea250a6ce30cf3f81533dc1b
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-12-05 09:55:44 +0100

    Edited exercise 0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,7 +8,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> int:
     """
     This async function will delay a return at a random time
     """
