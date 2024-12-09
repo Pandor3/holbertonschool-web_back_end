8e2ca2a5039d22fce0da18960a229fc66974fe76
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-12-09 10:12:22 +0100

    Added exercise 0 and its main file

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module will contain an asynchronous function which will generate
+random numbers for 10 loops and then wait 1 second between each loop
+"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    This function will asynchronously generate random numbers during
+    10 loops and wait 1 second between each loop
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
