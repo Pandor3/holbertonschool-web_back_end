73ff8f06c31d6817e422377014487f414c670360
Pandor3 <ThatDumbPhilosopher@gmail.com>
2025-01-20 15:01:49 +0100

    Edited exercise 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
+
 const rl = readline.createInterface({ input, output });
 
-    console.log('Welcome to Holberton School, what is your name?');
-    rl.question('', function(name) {
-        console.log(`Your name is : ${name}`);
-        console.log('This important software is now closing')
-        rl.close()
-    });
-
+console.log('Welcome to Holberton School, what is your name?');
+rl.question('', (name) => {
+  console.log(`Your name is : ${name}`);
+  console.log('This important software is now closing');
+  rl.close();
+});
