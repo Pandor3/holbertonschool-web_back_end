e3871b084d6d48d7602bcd31d5411c4f2dcf38b6
Pandor3 <ThatDumbPhilosopher@gmail.com>
2025-01-20 15:07:12 +0100

    Added a new line after the initial question

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ const { stdin: input, stdout: output } = require('node:process');
 
 const rl = readline.createInterface({ input, output });
 
-console.log('Welcome to Holberton School, what is your name?');
+console.log('Welcome to Holberton School, what is your name?\n');
 rl.question('', (name) => {
   console.log(`Your name is : ${name}`);
   console.log('This important software is now closing');
