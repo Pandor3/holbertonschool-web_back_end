16a5b0073ae6fcb3a3a685010328b518a8ff123d
Pandor3 <ThatDumbPhilosopher@gmail.com>
2025-01-22 15:48:35 +0100

    Edited exercise 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ const rl = readline.createInterface({ input, output });
 
 console.log('Welcome to Holberton School, what is your name?\n');
 rl.question('', (name) => {
-  console.log(`Your name is : ${name}`);
-  console.log('This important software is now closing');
+  console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing\n');
   rl.close();
 });
