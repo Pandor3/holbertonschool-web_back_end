689996aaaf24b95bfe341a9d8cdff614c616c517
Pandor3 <ThatDumbPhilosopher@gmail.com>
2025-01-22 11:19:05 +0100

    Edited exercise 4, there isn't just one solution in programmation though.

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,15 +2,17 @@ const http = require('http');
 
 // Ceci permet de créer un serveur HTTP et de définir le type de contenu de la réponse
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text-plain' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
   // Ceci envoie la réponse au client
   res.end('Hello Holberton School!');
 });
 
 // Ceci permet d'écouter sur le port 1245 et d'avoir une confirmation via un message sur le terminal
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 module.export = app;
