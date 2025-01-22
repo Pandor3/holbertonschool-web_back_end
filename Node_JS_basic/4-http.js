a7a229bea24faf136e74b2f610675b79d5cfffc7
Pandor3 <ThatDumbPhilosopher@gmail.com>
2025-01-22 11:12:41 +0100

    Added exercise 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+// Ceci permet de créer un serveur HTTP et de définir le type de contenu de la réponse
+const app = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text-plain'});
+
+    // Ceci envoie la réponse au client
+    res.end('Hello Holberton School!');
+});
+
+// Ceci permet d'écouter sur le port 1245 et d'avoir une confirmation via un message sur le terminal
+app.listen(1245, () => {
+    console.log('Server is listening on port 1245');
+});
+
+module.export = app;
